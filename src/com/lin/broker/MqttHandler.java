8cf1f6d7652f776c6fc88368e243d4e801c3ec0c
linsida1 <164078401@qq.com>
2017-01-23 07:47:03 +0800

    add code

--- src/com/lin/broker/MqttHandler.java
+++ src/com/lin/broker/MqttHandler.java
@@ -4,31 +4,13 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.mqtt.MqttMessage;
 
-import com.lin.exception.UnknowMqttMsgTypeException;
+import com.lin.handler.MqttMessageHandlerManager;
 
 public class MqttHandler extends SimpleChannelInboundHandler<MqttMessage> {
 
 	@Override
-	protected void channelRead0(ChannelHandlerContext ctx, MqttMessage msg)
-			throws Exception {
-		switch (msg.fixedHeader().messageType()) {
-		case CONNACK:
-			break;
-		case SUBACK:
-			break;
-		case UNSUBACK:
-			break;
-		case PUBLISH:
-			break;
-		case PUBACK:
-			break;
-		case PUBREC:
-			break;
-		case PUBCOMP:
-			break;
-		default:
-			throw new UnknowMqttMsgTypeException("mqtt msg type[" + msg.fixedHeader().messageType() + "]");
-		}
+	protected void channelRead0(ChannelHandlerContext ctx, MqttMessage msg) throws Exception {
+		MqttMessageHandlerManager.instance().handler(ctx, msg);
 
 	}
 }
